dd171cd2956398f3d035e71961232485687d2d4a
Pedro Barahona <pbarahona@syncroniza.net>
2023-06-08 16:27:11 -0400

    Evento Click

---
+++ WebFun/03_JS/03_1_EventonClick/scrip.js
@@ -0,0 +1,12 @@
+function changeLog (element){
+    element.innerText = "logOut";
+}
+
+function hide(element){
+    element.remove()
+};
+
+function customMessage(){
+console.log("ninjas are ok!!!")
+
+}
