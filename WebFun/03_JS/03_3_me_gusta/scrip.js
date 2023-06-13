9ff7b2bc49911b441a237b6b28beb9388d3ded9c
Pedro Barahona <pbarahona@syncroniza.net>
2023-06-13 12:00:08 -0400

    I like it

---
+++ WebFun/03_JS/03_3_me_gusta/scrip.js
@@ -0,0 +1,37 @@
+// // .......Neil Score......
+
+var score = 6;
+var countElement = document.querySelector("#countScore");
+console.log(countElement);
+
+
+function changeScore() { 
+    score ++;
+    countElement.innerHTML = score ;
+console.log(score);
+
+}
+
+// ...........Nicole.............................
+
+var Score1 = score + 6;
+var countElement1 =document.querySelector("#countScore1");
+console.log(countElement1);
+
+function cambiar_marcador() {
+    Score1 ++;
+    countElement1.innerHTML = Score1;
+
+}
+
+// .........Jim....................
+
+var Score2 = score + 3;
+var countElement2 =document.querySelector("#countScore2");
+console.log(countElement);
+
+function marcador_cambiado() {
+    Score2 ++;
+    countElement2.innerHTML = Score2;
+
+}
