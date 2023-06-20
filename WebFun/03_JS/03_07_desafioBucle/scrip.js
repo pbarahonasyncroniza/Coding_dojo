2944de1d9907b66df639d25ee36411931cfbdcd8
Pedro Barahona <pbarahona@syncroniza.net>
2023-06-20 10:17:19 -0400

    desafio Bucle

---
+++ WebFun/03_JS/03_07_desafioBucle/scrip.js
@@ -0,0 +1,35 @@
+// ejercicio 1 
+for (var x=1 ; x<=20 ; x++){
+    if(x % 2 !== 0){
+    console.log (x);}
+}
+
+// ejercicio 2 
+for (var y=1 ; y <=100; y++){
+    if(y % 3 == 0){
+        console.log(y)
+    }
+
+}
+
+// ejercicio 3
+for(var i =4 ; i>-4; i-=1.5){
+console.log (i);
+}
+
+// ejercicio 4
+var sum = 0;
+for ( var z= 1 ; z <=100; z++ ){
+   sum = sum + z
+    console.log(sum) 
+
+}
+
+// factoria 
+var product = 1;
+for (var w=1 ; w <=12 ; w++){
+  product = product*w
+  console.log(product);
+
+
+}
